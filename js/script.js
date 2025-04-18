473616a2912db3249178a2bd25f6a0aa7a8157b8
Guilherme Lopes Correa <guilhermelopescorrea171@gmail.com>
2025-04-17 21:04:27 -0300

    TESTE DO EMAIL

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 //consta é o objeto icone para trocar a cor do site
-const toggleTheme = document.getElementById("toggleTheme")
-const rootHtml = document.documentElement ; 
+const toggleTheme = document.getElementById("toggleTheme");
+const rootHtml = document.documentElement;
+const accordionHeader = document.querySelectorAll(".accordion-header");
 
 function changeTheme () {
 
-        const currentTheme = rootHtml.getAttribute ("data-theme") ;
+    const currentTheme = rootHtml.getAttribute("data-theme");
 
-        currentTheme === "light" ? rootHtml.setAttribute("data-theme" , "dark") : rootHtml.setAttribute("data-theme" , "light") ;
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
-        toggleTheme.classList.toggle("bi-sun")
-        toggleTheme.classList.toggle("bi-moon-stars")
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
 }
 
+toggleTheme.addEventListener("click", changeTheme);
 
-toggleTheme.addEventListener("click", changeTheme)
+//function accordion
+accordionHeader.forEach(header => {
+
+    header.addEventListener("click", () => {
+
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
